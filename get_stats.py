b4d4a01b45ae9765d91a178d1f3002d5a22708a1
SpiritedByte <SpiritedWine@outlook.com>
2020-06-01 23:49:39 +0100

    Improved multi-platform compatibility
    Using path module to get directory locations

--- get_stats.py
+++ get_stats.py
@@ -1,8 +1,8 @@
 import requests
 import os
-import csv
 import traceback
 import sys
+from pathlib import Path
 
 url_cases = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv'
 url_deaths = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-deaths_latest.csv'
@@ -11,73 +11,35 @@ url_deaths = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-deaths_l
 
 # create new folder to store the statistics
 def getStats():
-    cwd = os.getcwd()
-    # print(cwd)
-    if sys.platform.startswith('darwin') or sys.platform.startswith('linux2') or sys.platform.startswith('linux'):
-        new_folder = os.path.join(cwd, 'stats')
-        # print(new_folder)
-        # os.path.join(cwd, r'/second_part')
-        if not os.path.exists(new_folder):
-            os.makedirs(new_folder)
-        else:
-            pass
-        
-        print("Attempting to download COVID-19 cases...")
-        try:
-            response = requests.get(url_cases)
-            with open(os.path.join("stats", "covid-cases.csv"), 'wb') as f:
-                f.write(response.content)
-        except:
-            traceback.print_exc()
-            print("Failed to download COVID-19 cases.")
-        else:
-            print("Successful.")
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    path = Path("stats/")
+    path_to_stats = cwd / path
+    #print(path_to_stats)
 
-        print("Attempting to download COVID-19 deaths...")
-        try:
-            response = requests.get(url_deaths)
-            with open(os.path.join("stats", "covid-deaths.csv"), 'wb') as f:
-                f.write(response.content)
-        except:
-            print("Failed to download COVID-19 deaths.")
-        else:
-            print("Successful.")
-        
-    elif sys.platform.startswith('win32'):
-        new_folder = os.path.join(cwd, 'stats')
-        # print(new_folder)
-        # os.path.join(cwd, '/stats')
-        if not os.path.exists(new_folder):
-            os.makedirs(new_folder)
-        else:
-            pass
-
-        print("Attempting to download COVID-19 cases...")
-        try:
-            response = requests.get(url_cases)
-            with open(os.path.join("stats", "covid-cases.csv"), 'wb') as f:
-                f.write(response.content)
-        except:
-            print("Failed to download COVID-19 cases.")
-        else:
-            print("Successful.")
-        
-        print("Attempting to download COVID-19 deaths...")
-        try:
-            response = requests.get(url_deaths)
-            with open(os.path.join("stats", "covid-deaths.csv"), 'wb') as f:
-                f.write(response.content)
-        except:
-            print("Failed to download COVID-19 deaths.")
-        else:
-            print("Successful.")
-
-        
+    if not os.path.exists(path_to_stats):
+        os.makedirs(path_to_stats)
     else:
-        print("Sorry, I don't know what your OS is :(")
-        sys.exit()
+        pass
+    
+    print("Attempting to download COVID-19 cases...")
+    try:
+        response = requests.get(url_cases)
+        with open(os.path.join(path_to_stats, "covid-cases.csv"), 'wb') as f:
+            f.write(response.content)
+    except:
+        traceback.print_exc()
+        print("Failed to download COVID-19 cases.")
+    else:
+        print("Successful.")
 
+    print("Attempting to download COVID-19 deaths...")
+    try:
+        response = requests.get(url_deaths)
+        with open(os.path.join(path_to_stats, "covid-deaths.csv"), 'wb') as f:
+            f.write(response.content)
+    except:
+        print("Failed to download COVID-19 deaths.")
+    else:
+        print("Successful.")
 
-
-
-
+getStats()
