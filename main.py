62fccba7516be6faf8fc5013110a2d847e508416
SpiritedByte <SpiritedWine@outlook.com>
2020-06-07 23:14:51 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# IMPORT NECESSARY MODULES
-import matplotlib.pyplot as plt
-import csv
+# Import functions as modules from other .py files
 from get_stats import getStats
 from stats_parser import csv_parser
-import os
+from make_graph import cases_plot # Need to make deaths plot too
 
+# First, get latest stats using getStats()
 getStats()
-# csv_parser()
+
+# Then, parse the data: we only need the rows with 'England', and the date and cumulative values
+imported = csv_parser()
+cases_filtered_import = imported[0]
+deaths_filtered_import = imported[1]
+
+# Finally, make the graph
+cases_plot(cases_filtered_import)
