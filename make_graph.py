b020120913cb1ea99b5c9c7d538d8329789a019b
SpiritedByte <SpiritedWine@outlook.com>
2020-06-24 12:44:20 +0100

    Merged graphs and changed to bar graph
    Need to change death values to more representative of the scale of cases. Plus, need to fix overlapping of X-axis values.

--- make_graph.py
+++ make_graph.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 
 def plot(cases,deaths):
-    cases.plot(x='Specimen date',y='Cumulative lab-confirmed cases',color='green')
-    deaths.plot(x='Reporting date',y='Cumulative deaths',color='red')
+    #cases.plot(x='Specimen date',y='Cumulative lab-confirmed cases',color='green')
+    #deaths.plot(x='Reporting date',y='Cumulative deaths',color='red')
+    plt.figure("Cumulative deaths and cases")
+    ax = plt.gca()
+    cases.plot(kind='bar', x='Specimen date', y='Cumulative lab-confirmed cases',ax=ax, color='green')
+    deaths.plot(kind='bar', x='Reporting date', y='Cumulative deaths', ax=ax, color='red')
     plt.show()
